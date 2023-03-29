fba2b76d9bf6849bea523fec43dbd77dd70a0fb4
Andrew Cai <ayc57@cornell.edu>
2023-03-28 23:49:41 -0400

    fdsf

--- drum/hps/param_reader.c
+++ drum/hps/param_reader.c
@@ -13,31 +13,44 @@ Write your code in this editor and press "Run" button to compile and execute it.
 float center_peak = 0.75;
 int rows = 30;
 int cols = 30;
+int damping = 9;
+int tension = 4;
 
 int main()
 {
     char input;
     while(1){
-        printf("c to change params, p to play drum: \n");
+        printf("change params: \n p for peak value, r for rows, c for cols, d for damping, g for tension\n");
         scanf("%c", &input);
-        if (input == 'c'){
+        if (input == 'p'){
             float new_center_peak;
             printf("enter center peak: \n");
             scanf("%f", &new_center_peak);
             center_peak = new_center_peak;
-            
+        
+        } else if (input == 'r'){
             int new_rows;
             printf("enter rows: \n");
             scanf("%d", &new_rows);
             rows = new_rows;
             
+        } else if (input == 'c'){
             int new_cols;
             printf("enter cols: \n");
             scanf("%d", &new_cols);
             cols = new_cols;
         }
-        else if (input == 'p'){
-            printf("DO PLAY with row = %d, col = %d, peak = %f \n", rows, cols, center_peak);
+        else if (input == 'd'){
+            int new_damping;
+            printf("enter damping: \n");
+            scanf("%f", &new_damping);
+            damping = new_damping;
+        }
+        else if (input == 'g'){
+            int new_tension;
+            printf("enter tension: \n");
+            scanf("%f", &new_tension);
+            tension = new_tension;
         }
     }
     return 0;
--- drum/hps/push_out_init_vals.c
+++ drum/hps/push_out_init_vals.c
@@ -21,23 +21,100 @@
 #define HW_REGS_BASE          0xff200000
 //#define HW_REGS_SPAN        0x00200000 
 #define HW_REGS_SPAN          0x00005000 
+#define FIXED_CONSTANT = 131072
+
+float center_peak = 0.75;
+int rows = 30;
+int cols = 30;
+int damping = 9;
+int tension = 4;
 
 // ODE solver PIO base addresses
-#define FPGA_ACK              0x00000000
-#define FPGA_RDY              0x00000010
-#define FPGA_OUT              0x00000020
-#define FPGA_ROWS             0x00000010
-#define FPGA_RHO_GAIN         0x00000020
+#define FPGA_CENTER_PEAK              0x00000000
+#define FPGA_COLS                     0x00000010
+#define FPGA_ROWS                     0x00000020
+#define FPGA_DAMPING                  0x00000010
+#define FPGA_TENSION                  0x00000020
+#define FPGA_INCR                     0x00000030
+void *h2p_lw_virtual_base;
 
+// character buffer
+volatile unsigned int * vga_char_ptr = NULL ;
+void *vga_char_virtual_base;
 volatile unsigned int * fpga_ack_ptr = NULL ;
 volatile unsigned int * fpga_rdy_ptr = NULL ;
 volatile unsigned int * fpga_out_ptr = NULL ;
 volatile unsigned int * fpga_rows_ptr = NULL ;
 volatile unsigned int * fpga_rho_gain_ptr = NULL;
 
+void initPtrs(){
+    // === need to mmap: =======================
+	// FPGA_CHAR_BASE
+	// FPGA_ONCHIP_BASE      
+	// HW_REGS_BASE        
+  
+	// === get FPGA addresses ==================
+    // Open /dev/mem
+	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) 	{
+		printf( "ERROR: could not open \"/dev/mem\"...\n" );
+		return( 1 );
+	}
 
-fpga_ack_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ACK); //add offset to base address to get ACK PTR
-fpga_rdy_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RDY); 
-fpga_out_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_OUT); 
-fpga_rows_ptr     = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ROWS); 
-fpga_rho_gain_ptr = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RHO_GAIN); 
+    // get virtual addr that maps to physical
+	h2p_lw_virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );	
+	if( h2p_lw_virtual_base == MAP_FAILED ) {
+		printf( "ERROR: mmap1() failed...\n" );
+		close( fd );
+		return(1);
+	}
+    fpga_ack_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ACK); //add offset to base address to get ACK PTR
+    fpga_rdy_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RDY); 
+    fpga_out_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_OUT); 
+    fpga_rows_ptr     = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ROWS); 
+    fpga_rho_gain_ptr = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RHO_GAIN); 
+
+}
+
+void generateIncr()
+
+int main()
+{
+    initPtrs();
+    char input;
+    while(1){
+        printf("change params: \n p for peak value, r for rows, c for cols, d for damping, g for tension\n");
+        scanf("%c", &input);
+        if (input == 'p'){
+            float new_center_peak;
+            printf("enter center peak: \n");
+            scanf("%f", &new_center_peak);
+            center_peak = new_center_peak;
+        
+        } else if (input == 'r'){
+            int new_rows;
+            printf("enter rows: \n");
+            scanf("%d", &new_rows);
+            rows = new_rows;
+            
+        } else if (input == 'c'){
+            int new_cols;
+            printf("enter cols: \n");
+            scanf("%d", &new_cols);
+            cols = new_cols;
+        }
+        else if (input == 'd'){
+            int new_damping;
+            printf("enter damping: \n");
+            scanf("%f", &new_damping);
+            damping = new_damping;
+        }
+        else if (input == 'g'){
+            int new_tension;
+            printf("enter tension: \n");
+            scanf("%f", &new_tension);
+            tension = new_tension;
+        }
+    }
+    return 0;
+}
+
