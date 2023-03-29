0c622768b26201578519332291075a477edd414c
Andrew <cai.andrew1226@gmail.com>
2023-03-29 02:36:05 -0400

    fdsf

--- drum/hps/push_out_init_vals.c
+++ drum/hps/push_out_init_vals.c
@@ -1,3 +1,11 @@
+///////////////////////////////////////
+/// 640x480 version! 16-bit color
+/// This code will segfault the original
+/// DE1 computer
+/// compile with
+/// gcc push_out_init_vals.c -o drum -O2 -lm
+///
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -24,30 +32,32 @@
 #define FIXED_CONSTANT = 131072
 
 float center_peak = 0.75;
-int rows = 30;
-int cols = 30;
-int damping = 9;
+int rows = 60;
+int cols = 60;
+int damping = 11;
 int tension = 4;
+int incr = 0xDA;
 
 // ODE solver PIO base addresses
 #define FPGA_CENTER_PEAK              0x00000000
 #define FPGA_COLS                     0x00000010
 #define FPGA_ROWS                     0x00000020
-#define FPGA_DAMPING                  0x00000010
-#define FPGA_TENSION                  0x00000020
-#define FPGA_INCR                     0x00000030
+#define FPGA_DAMPING                  0x00000030
+#define FPGA_TENSION                  0x00000040
+#define FPGA_INCR                     0x00000050
 void *h2p_lw_virtual_base;
-
+int fd;
 // character buffer
 volatile unsigned int * vga_char_ptr = NULL ;
 void *vga_char_virtual_base;
-volatile unsigned int * fpga_ack_ptr = NULL ;
-volatile unsigned int * fpga_rdy_ptr = NULL ;
-volatile unsigned int * fpga_out_ptr = NULL ;
-volatile unsigned int * fpga_rows_ptr = NULL ;
-volatile unsigned int * fpga_rho_gain_ptr = NULL;
+volatile unsigned int * center_peak_ptr = NULL ;
+volatile unsigned int * col_ptr         = NULL ;
+volatile unsigned int * row_ptr         = NULL ;
+volatile unsigned int * damping_ptr     = NULL ;
+volatile unsigned int * tension_ptr     = NULL ;
+volatile unsigned int * incr_ptr        = NULL ;
 
-void initPtrs(){
+int initPtrs(){
     // === need to mmap: =======================
 	// FPGA_CHAR_BASE
 	// FPGA_ONCHIP_BASE      
@@ -67,15 +77,15 @@ void initPtrs(){
 		close( fd );
 		return(1);
 	}
-    fpga_ack_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ACK); //add offset to base address to get ACK PTR
-    fpga_rdy_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RDY); 
-    fpga_out_ptr      = (unsigned int * ) (h2p_lw_virtual_base + FPGA_OUT); 
-    fpga_rows_ptr     = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ROWS); 
-    fpga_rho_gain_ptr = (unsigned int * ) (h2p_lw_virtual_base + FPGA_RHO_GAIN); 
+
+    center_peak_ptr   = (unsigned int * ) (h2p_lw_virtual_base + FPGA_CENTER_PEAK);
+    col_ptr           = (unsigned int * ) (h2p_lw_virtual_base + FPGA_COLS);
+    row_ptr           = (unsigned int * ) (h2p_lw_virtual_base + FPGA_ROWS);
+    damping_ptr       = (unsigned int * ) (h2p_lw_virtual_base + FPGA_DAMPING);
+    tension_ptr       = (unsigned int * ) (h2p_lw_virtual_base + FPGA_TENSION);
+    incr_ptr          = (unsigned int * ) (h2p_lw_virtual_base + FPGA_INCR);
 
 }
-
-void generateIncr()
 
 int main()
 {
@@ -83,37 +93,66 @@ int main()
     char input;
     while(1){
         printf("change params: \n p for peak value, r for rows, c for cols, d for damping, g for tension\n");
-        scanf("%c", &input);
-        if (input == 'p'){
-            float new_center_peak;
-            printf("enter center peak: \n");
-            scanf("%f", &new_center_peak);
-            center_peak = new_center_peak;
-        
-        } else if (input == 'r'){
-            int new_rows;
-            printf("enter rows: \n");
-            scanf("%d", &new_rows);
-            rows = new_rows;
-            
-        } else if (input == 'c'){
-            int new_cols;
-            printf("enter cols: \n");
-            scanf("%d", &new_cols);
-            cols = new_cols;
+        if (scanf("%c", &input)){
+            if (input == 'p'){
+                float new_center_peak;
+                printf("enter center peak: \n");
+                if (scanf("%f", &new_center_peak)){
+                    center_peak = new_center_peak;
+                }
+                else{
+                    printf("error reading input");
+                }
+            } else if (input == 'r'){
+                int new_rows;
+                printf("enter rows: \n");
+                if (scanf("%d", &new_rows)){
+                    rows = new_rows;
+                }
+                else{
+                    printf("error reading input");
+                } 
+            } else if (input == 'c'){
+                int new_cols;
+                printf("enter cols: \n");
+                if (scanf("%d", &new_cols)){
+                    cols = new_cols;
+                }
+                else{
+                    printf("error reading input");
+                }
+            }
+            else if (input == 'd'){
+                int new_damping;
+                printf("enter damping: \n");
+                if (scanf("%d", &new_damping)){
+                    damping = new_damping;
+                }
+                else{
+                    printf("error reading input");
+                }
+            }
+            else if (input == 'g'){
+                int new_tension;
+                printf("enter tension: \n");
+                if (scanf("%d", &new_tension)){
+                    tension = new_tension;
+                }
+                else{
+                    printf("error reading input");
+                }
+            }
+
         }
-        else if (input == 'd'){
-            int new_damping;
-            printf("enter damping: \n");
-            scanf("%f", &new_damping);
-            damping = new_damping;
+        else{
+            printf("error reading input");
         }
-        else if (input == 'g'){
-            int new_tension;
-            printf("enter tension: \n");
-            scanf("%f", &new_tension);
-            tension = new_tension;
-        }
+        *(center_peak_ptr) = (int) (center_peak);
+        *(col_ptr) = cols;
+        *(row_ptr) = rows;
+        *(damping_ptr) = damping;
+        *(tension_ptr) = tension;
+        *(incr_ptr) = incr;
     }
     return 0;
 }
